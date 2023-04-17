d63ec81600203ea761b9470dcfd5c27bf800f788
niaa <61405796+qtqqt@users.noreply.github.com>
2023-04-17 11:40:27 +0800

    ignore

---
+++ administrator/cache/_media_version/1bd797b80c0951539dc239c6119cdef1-cache-_media_version-0c39ed5128293f919829be5543dd77f4.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:32:"1925f40671d9fed29aff06485b2116fa";s:6:"output";s:0:"";}
---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-04-13 04:52:51 UTC
+#Software: Joomla! 4.2.9 Stable [ Uaminifu ] 14-March-2023 15:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-04-13T04:52:51+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2023-04-13T04:54:37+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
